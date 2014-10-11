cf4ccb70173d7bd4f64afbbaed7fca4074a44a3b
alonmack <alonmack@gmail.com>
2014-10-11 14:25:21 +0300

    create structure EV and add first test

---
+++ app/src/androidTest/java/ua/umbrella/englishverb/MyFirstTestActivityTest.java
@@ -0,0 +1,43 @@
+package ua.umbrella.englishverb;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.TextView;
+
+import ua.umbrella.englishverb.R;
+import ua.umbrella.englishverb.GameActivity;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class MyFirstTestActivityTest
+    extends ActivityInstrumentationTestCase2<GameActivity>
+{
+  private GameActivity mFirstTestActivity;
+  private TextView mFirstTestText;
+
+  public MyFirstTestActivityTest()
+  {
+    super(GameActivity.class);
+  }
+
+  @Override
+  protected void setUp() throws Exception
+  {
+    super.setUp();
+    mFirstTestActivity = getActivity();
+    mFirstTestText = (TextView) mFirstTestActivity.findViewById(R.id.tv);
+  }
+
+  public void testPreconditions()
+  {
+    assertNotNull("mFirstTestActivity is null", mFirstTestActivity);
+    assertNotNull("mFirstTestText is null", mFirstTestText);
+  }
+
+  public void testMyFirstTestTextView_labelText()
+  {
+    final String expected = mFirstTestActivity.getString(R.string.hello_world);
+    final String actual = mFirstTestText.getText().toString();
+    assertEquals(expected, actual);
+  }
+}
