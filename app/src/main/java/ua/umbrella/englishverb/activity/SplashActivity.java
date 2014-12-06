a117f0582b5ada0e193b90cfbd007e00ee25ee83
alonmack <alonmack@gmail.com>
2014-12-06 13:09:47 +0200

    gradle setting

--- app/src/main/java/ua/umbrella/englishverb/activity/SplashActivity.java
+++ app/src/main/java/ua/umbrella/englishverb/activity/SplashActivity.java
@@ -3,6 +3,8 @@ package ua.umbrella.englishverb.activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 
 import ua.umbrella.englishverb.R;
 import ua.umbrella.englishverb.service.VerbService;
